db9a0abd44409d14f7802baa72ffc47d3c5aabbb
daniel <daniel.kuehlwein@gmail.com>
2013-07-17 11:53:40 +0200

    cleanup

--- MaLeS/learn.py
+++ MaLeS/learn.py
@@ -89,112 +89,108 @@ if __name__ == '__main__':
 
     # Create initial models
     logger.info('Creating Models.')
-    if os.path.isfile(config.get('Learn', 'StrategiesFile') ) and os.path.isfile(config.get('Learn', 'KernelFile') ) and not config.getboolean('Settings', 'Clear') :
-        startStrategies,startTime,strategies,notSolvedYet,kernelGrid = load_data(config.get('Learn', 'StrategiesFile') )        
-        KMs = load_data(config.get('Learn', 'KernelFile') )       
-    else:
-        # Load Strategies
-        logger.info("Parsing Strategy Files.")
-        tmpStrategies = load_strategies(config.get('Settings', 'ResultsDir'),config.getfloat('Learn', 'CPU Bias'))  
-    
-        logger.info("Deleting dominated strategies.")
-        # Get best times
-        bestTimes = {}
-        for s in tmpStrategies:
-            for p,t in s.solvedProblems.iteritems():
-                if bestTimes.has_key(p):
-                    if bestTimes[p] > t:
-                        bestTimes[p] = t
-                else:
-                    bestTimes[p] = t
-        bestStrats = {}
-        for s in tmpStrategies:
-            for p,t in s.solvedProblems.iteritems():
-                bestTime = bestTimes[p]                                    
-                if bestStrats.has_key(p):                    
-                    solvedByBest = len(bestStrats[p][0].solvedProblems)
-                    if t < bestTime + config.getfloat('Learn', 'Tolerance'):
-                        if solvedByBest < len(s.solvedProblems):
-                            bestStrats[p] = [s]
-                        elif solvedByBest == len(s.solvedProblems):
-                            bestStrats[p].append(s)
-                elif t < bestTime + config.getfloat('Learn', 'Tolerance'):                
-                    bestStrats[p] = [s]
-        tmp2Strategies = []
-        for sList in bestStrats.values():
-            for s in sList:
-                tmp2Strategies.append(s)
-        tmp2Strategies = set(tmp2Strategies)
-        logger.info('Deleted %s strategies. %s strategies left.' % (len(tmpStrategies)-len(tmp2Strategies),len(tmp2Strategies)))        
-       
-        # Get start strategies
-        logger.info("Getting starting strategies..")
-        startStrategies,solved,notSolvedYet = greedy_startStrategies(tmp2Strategies,runTime=config.getfloat('Learn', 'StartStrategiesTime'),number=config.getint('Learn', 'StartStrategies') )
-        
-        # Delete all problem that were solved by startStrategies and all strategies that solve none of the leftover problems.
-        logger.info("Deleting all solved problems.")
-        manager = Manager()    
-        strategies = manager.list([])
-        for s in tmp2Strategies:
-            oldSolved = s.solvedProblems
-            s.solvedProblems = dict([(i,oldSolved[i]) for i in notSolvedYet if i in oldSolved])
-            if len(s.solvedProblems) > 0:
-                strategies.append(s)
+    # Load Strategies
+    logger.info("Parsing Strategy Files.")
+    tmpStrategies = load_strategies(config.get('Settings', 'ResultsDir'),config.getfloat('Learn', 'CPU Bias'))  
 
-        """
-        # TODO: Hack
+    logger.info("Deleting dominated strategies.")
+    # Get best times
+    bestTimes = {}
+    for s in tmpStrategies:
+        for p,t in s.solvedProblems.iteritems():
+            if bestTimes.has_key(p):
+                if bestTimes[p] > t:
+                    bestTimes[p] = t
+            else:
+                bestTimes[p] = t
+    bestStrats = {}
+    for s in tmpStrategies:
+        for p,t in s.solvedProblems.iteritems():
+            bestTime = bestTimes[p]                                    
+            if bestStrats.has_key(p):                    
+                solvedByBest = len(bestStrats[p][0].solvedProblems)
+                if t < bestTime + config.getfloat('Learn', 'Tolerance'):
+                    if solvedByBest < len(s.solvedProblems):
+                        bestStrats[p] = [s]
+                    elif solvedByBest == len(s.solvedProblems):
+                        bestStrats[p].append(s)
+            elif t < bestTime + config.getfloat('Learn', 'Tolerance'):                
+                bestStrats[p] = [s]
+    tmp2Strategies = []
+    for sList in bestStrats.values():
+        for s in sList:
+            tmp2Strategies.append(s)
+    tmp2Strategies = set(tmp2Strategies)
+    logger.info('Deleted %s strategies. %s strategies left.' % (len(tmpStrategies)-len(tmp2Strategies),len(tmp2Strategies)))        
+   
+    # Get start strategies
+    logger.info("Getting starting strategies..")
+    startStrategies,solved,notSolvedYet = greedy_startStrategies(tmp2Strategies,runTime=config.getfloat('Learn', 'StartStrategiesTime'),number=config.getint('Learn', 'StartStrategies') )
+    
+    # Delete all problem that were solved by startStrategies and all strategies that solve none of the leftover problems.
+    logger.info("Deleting all solved problems.")
+    manager = Manager()    
+    strategies = manager.list([])
+    for s in tmp2Strategies:
+        oldSolved = s.solvedProblems
+        s.solvedProblems = dict([(i,oldSolved[i]) for i in notSolvedYet if i in oldSolved])
+        if len(s.solvedProblems) > 0:
+            strategies.append(s)
+
+    """
+    # TODO: Hack
+    for i,p in enumerate(notSolvedYet):
+        p = p.replace('/scratch/kuehlwein','/home/daniel/TPTP')
+        notSolvedYet[i] = p
+    #"""
+    # Load Problem Features
+    if os.path.exists(config.get('Learn', 'FeaturesFile') ) and not config.getboolean('Settings', 'Clear') :
+        logger.info('Loading featureFile')
+        featureDict,minVals,maxVals = load_data(config.get('Learn', 'FeaturesFile') )
+    else:
+        logger.info('Creating feature Dict.')
+        featureDict,maxVals,minVals = compute_features(notSolvedYet,config.get('Learn', 'Features'),config.getint('Settings', 'Cores'))            
+        featureDict = normalize_featureDict(featureDict,maxVals,minVals)
+        """        
+        # TODO: HACK!
+        aaa = {}
+        for key,val in featureDict.iteritems():
+            key = key.replace('/home/daniel/TPTP','/scratch/kuehlwein')
+            aaa[key] = val
+        featureDict = aaa
+        
         for i,p in enumerate(notSolvedYet):
-            p = p.replace('/scratch/kuehlwein','/home/daniel/TPTP')
+            p = p.replace('/home/daniel/TPTP','/scratch/kuehlwein')
             notSolvedYet[i] = p
+        # HACK END
         #"""
-        # Load Problem Features
-        if os.path.exists(config.get('Learn', 'FeaturesFile') ) and not config.getboolean('Settings', 'Clear') :
-            logger.info('Loading featureFile')
-            featureDict,minVals,maxVals = load_data(config.get('Learn', 'FeaturesFile') )
-        else:
-            logger.info('Creating feature Dict.')
-            featureDict,maxVals,minVals = compute_features(notSolvedYet,config.get('Learn', 'Features'),config.getint('Settings', 'Cores'))            
-            featureDict = normalize_featureDict(featureDict,maxVals,minVals)
-            """        
-            # TODO: HACK!
-            aaa = {}
-            for key,val in featureDict.iteritems():
-                key = key.replace('/home/daniel/TPTP','/scratch/kuehlwein')
-                aaa[key] = val
-            featureDict = aaa
-            
-            for i,p in enumerate(notSolvedYet):
-                p = p.replace('/home/daniel/TPTP','/scratch/kuehlwein')
-                notSolvedYet[i] = p
-            # HACK END
-            #"""
-            dump_data((featureDict,minVals,maxVals),config.get('Learn', 'FeaturesFile') )
-            logger.info('Done') 
-        
-        # Create joint kernel matrices
-        logger.info('Creating kernel matrices.')
-        pool = Pool(processes = cpu_count())
-        KMs = pool.map(create_application_matrix_star, itertools.izip(itertools.repeat(notSolvedYet),\
-                                                                      itertools.repeat(notSolvedYet),\
-                                                                      itertools.repeat(featureDict),\
-                                                                      kernelGrid))       
-        pool.close()
-        pool.join()  
-        dump_data(KMs,config.get('Learn', 'KernelFile') )
-        logger.info('Done')
-        
-        logger.info('Learning strategy models.')        
-        pool = Pool(processes = cpu_count())
-        strategies = pool.map(apply_strat,itertools.izip(strategies,\
-                                                         itertools.repeat(notSolvedYet),\
-                                                         itertools.repeat(KMs),\
-                                                         itertools.repeat(regGrid),\
-                                                         itertools.repeat(config.getboolean('Learn', 'CrossValidate')),\
-                                                         itertools.repeat(config.getint('Learn', 'CrossValidationFolds'))))
-        pool.close()
-        pool.join()  
-        dump_data((strategies,notSolvedYet,kernelGrid),config.get('Learn', 'StrategiesFile') )
-        dump_data((startStrategies,config.getfloat('Learn', 'StartStrategiesTime')),config.get('Learn', 'StrategiesFile')+'startStrats' )
+        dump_data((featureDict,minVals,maxVals),config.get('Learn', 'FeaturesFile') )
+        logger.info('Done') 
+    
+    # Create joint kernel matrices
+    logger.info('Creating kernel matrices.')
+    pool = Pool(processes = cpu_count())
+    KMs = pool.map(create_application_matrix_star, itertools.izip(itertools.repeat(notSolvedYet),\
+                                                                  itertools.repeat(notSolvedYet),\
+                                                                  itertools.repeat(featureDict),\
+                                                                  kernelGrid))       
+    pool.close()
+    pool.join()  
+    dump_data(KMs,config.get('Learn', 'KernelFile') )
+    logger.info('Done')
+    
+    logger.info('Learning strategy models.')        
+    pool = Pool(processes = cpu_count())
+    strategies = pool.map(apply_strat,itertools.izip(strategies,\
+                                                     itertools.repeat(notSolvedYet),\
+                                                     itertools.repeat(KMs),\
+                                                     itertools.repeat(regGrid),\
+                                                     itertools.repeat(config.getboolean('Learn', 'CrossValidate')),\
+                                                     itertools.repeat(config.getint('Learn', 'CrossValidationFolds'))))
+    pool.close()
+    pool.join()  
+    dump_data((strategies,notSolvedYet,kernelGrid),config.get('Learn', 'StrategiesFile') )
+    dump_data((startStrategies,config.getfloat('Learn', 'StartStrategiesTime')),config.get('Learn', 'StrategiesFile')+'startStrats' )
     logger.info('All Done.')    
                      
                      
